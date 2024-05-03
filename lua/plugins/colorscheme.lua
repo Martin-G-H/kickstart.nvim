3bb8bf321d87fc7a062ac707c0ce5632f00274cd
Martin Hultgren <martin.g.hultgren@gmail.com>
2024-05-03 18:05:11 +0200

    Fix macchiato name

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,7 +9,6 @@ return { -- You can easily change to a different colorscheme.
   init = function()
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
-    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
     vim.cmd.colorscheme 'catppuccin-macchiato'
 
     -- You can configure highlights by doing something like
